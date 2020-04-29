7d869f7966630839ee884d2af908979c81d8956f
MihailKot-GeekBrains <64553119+MihailKot-GeekBrains@users.noreply.github.com>
2020-04-29 23:35:37 +0500

    Create project

---
+++ HomeWork1/HomeWork1/ClassHelp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    static class ClassHelp
+    {
+        public static void Print(string ms, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(ms);
+        }
+
+        public static void Pause(string ms = "")
+        {
+            Console.WriteLine(ms);
+            Console.ReadKey();
+        }
+    }
+}
